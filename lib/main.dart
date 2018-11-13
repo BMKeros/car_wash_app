ff80d437fca1df0050d08f0d37eb5ef37c6ba390
Antony Carrizo (boanergepro) <antonycarrizo96@gmail.com>
2018-11-13 17:51:34 -0400

    [MOD]: - Seteado el apikey de google maps al Widget MapView.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:panelmex_app/routes.dart';
 import 'package:panelmex_app/screens/login.dart';
+import 'package:map_view/map_view.dart';
+import 'package:panelmex_app/config/config.dart';
 
 void main() {
-runApp(new MaterialApp(
+  MapView.setApiKey(APIKEY);
+  runApp(new MaterialApp(
     home: Login(),
     debugShowCheckedModeBanner: false,
     theme: ThemeData(
