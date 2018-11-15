96825e08ed23788f2688afc3f08025c09cd13d32
Antony Carrizo (boanergepro) <antonycarrizo96@gmail.com>
2018-11-15 16:46:34 -0400

    [ADD]: - Credada vista para ver los detalles del producto.

---
+++ lib/screens/client/service_detail.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ServiceDeatil extends StatefulWidget {
+  @override
+  _ServiceDeatilState createState() => new _ServiceDeatilState();
+ }
+class _ServiceDeatilState extends State<ServiceDeatil> {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+  
+    );
+  }
+}
