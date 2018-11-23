0d4d3dd9530c96812dbae465ab07e1974a4dfa30
Antony Carrizo (boanergepro) <antonycarrizo96@gmail.com>
2018-11-23 13:34:34 +0000

    [ADD]: - Agregado directorio para almacenar funciones utiles.

--- lib/common/utils.dart
+++ lib/common/utils.dart
@@ -1,4 +1,4 @@
-String getStaticMapBox(String latitud, String longitud, String height, String width) {
-  return 'https://api.mapbox.com/styles/v1/mapbox/streets-v10/static/${latitud},${longitud},7.7,0,0/${height}x${width}?access_token=pk.eyJ1IjoiaG9zdGVsaXgiLCJhIjoiY2pvc3pjMzk1MHZ5MTNxbmhicGFmbWpwcCJ9.Q6iOP5hPtKHNlbJWup_r3g';
+String getStaticMapBox(double latitud, double longitud, String height, String width) {
+  return 'https://api.mapbox.com/styles/v1/mapbox/streets-v10/static/${longitud},${latitud},14.0,0,0/${height}x${width}?access_token=pk.eyJ1IjoiaG9zdGVsaXgiLCJhIjoiY2pvc3pjMzk1MHZ5MTNxbmhicGFmbWpwcCJ9.Q6iOP5hPtKHNlbJWup_r3g';
 }
 //https://api.mapbox.com/styles/v1/mapbox/streets-v10/static/-75.12209,0.18242,7.7,0,0/300x200?access_token=pk.eyJ1IjoiaG9zdGVsaXgiLCJhIjoiY2pvc3pjMzk1MHZ5MTNxbmhicGFmbWpwcCJ9.Q6iOP5hPtKHNlbJWup_r3g
