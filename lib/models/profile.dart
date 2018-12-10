3b59d4bcfb7949ded5bf09efd2fd7d340486846d
Israel Lugo <hostelixisrael@gmail.com>
2018-12-10 17:39:29 -0400

    [MOD] agregado nuevos campos al modelo de profile

--- lib/models/profile.dart
+++ lib/models/profile.dart
@@ -1,12 +1,36 @@
+import 'package:firebase_database/firebase_database.dart';
+
 class Profile {
-  String email;
-  String firstName;
-  String lastName;
-  String address;
-  String phoneNumber;
+  String _key;
+  String _first_name;
+  String _last_name;
+  String _image_url;
+  String _address;
+  String _phone_number;
+
+  String get key => _key;
+
+  String get firstName => _first_name;
+
+  String get lastName => _last_name;
+
+  String get imageUrl => _image_url;
+
+  String get address => _address;
+
+  String get phoneNumber => _phone_number;
 
   @override
   String toString() {
-    return "$firstName $lastName";
+    return "$_first_name} ${_last_name}";
+  }
+
+  Profile.fromSnapshot(DataSnapshot snapshot) {
+    _key = snapshot.key;
+    _address = snapshot.value['address'];
+    _first_name = snapshot.value['first_name'];
+    _last_name = snapshot.value['last_name'];
+    _image_url = snapshot.value['image_url'];
+    _phone_number = snapshot.value['phone_number'];
   }
 }
