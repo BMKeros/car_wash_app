ff5832845b00dc0fb2fb4c95bca52051a3797790
Israel Lugo <hostelixisrael@gmail.com>
2018-12-10 18:20:24 -0400

    [MOD] se cambio logica en fromsnapshot

--- lib/models/profile.dart
+++ lib/models/profile.dart
@@ -8,6 +8,9 @@ class Profile {
   String _address;
   String _phone_number;
 
+  Profile(this._key, this._first_name, this._last_name, this._image_url,
+      this._phone_number, this._address);
+
   String get key => _key;
 
   String get firstName => _first_name;
@@ -16,21 +19,23 @@ class Profile {
 
   String get imageUrl => _image_url;
 
+  bool get hasImage => _image_url != null && _image_url.isNotEmpty;
+
   String get address => _address;
 
   String get phoneNumber => _phone_number;
 
   @override
   String toString() {
-    return "$_first_name} ${_last_name}";
+    return "$_first_name $_last_name";
   }
 
   Profile.fromSnapshot(DataSnapshot snapshot) {
     _key = snapshot.key;
-    _address = snapshot.value['address'];
-    _first_name = snapshot.value['first_name'];
-    _last_name = snapshot.value['last_name'];
-    _image_url = snapshot.value['image_url'];
-    _phone_number = snapshot.value['phone_number'];
+    _address = snapshot.value['address'] ?? null;
+    _first_name = snapshot.value['first_name'] ?? null;
+    _last_name = snapshot.value['last_name'] ?? null;
+    _image_url = snapshot.value['image_url'] ?? null;
+    _phone_number = snapshot.value['phone_number'] ?? null;
   }
 }
