493d276a65183fbe2f41e007c5080435f4c245a5
Israel Lugo <hostelixisrael@gmail.com>
2018-12-14 12:21:22 -0400

    [MOD] agregado markador al static map

--- lib/common/utils.dart
+++ lib/common/utils.dart
@@ -1,7 +1,7 @@
 import 'package:panelmex_app/common/constans.dart';
 
 String getStaticMapBox(double latitud, double longitud, String height, String width) {
-  return 'https://api.mapbox.com/styles/v1/mapbox/streets-v10/static/${longitud},${latitud},14.0,0,0/${height}x${width}?access_token=pk.eyJ1IjoiaG9zdGVsaXgiLCJhIjoiY2pvc3pjMzk1MHZ5MTNxbmhicGFmbWpwcCJ9.Q6iOP5hPtKHNlbJWup_r3g';
+  return 'https://api.mapbox.com/styles/v1/mapbox/streets-v10/static/pin-l+00F(${longitud},${latitud})/${longitud},${latitud},14.0,0,0/${height}x${width}?access_token=pk.eyJ1IjoiaG9zdGVsaXgiLCJhIjoiY2pvc3pjMzk1MHZ5MTNxbmhicGFmbWpwcCJ9.Q6iOP5hPtKHNlbJWup_r3g';
 }
 //https://api.mapbox.com/styles/v1/mapbox/streets-v10/static/-75.12209,0.18242,7.7,0,0/300x200?access_token=pk.eyJ1IjoiaG9zdGVsaXgiLCJhIjoiY2pvc3pjMzk1MHZ5MTNxbmhicGFmbWpwcCJ9.Q6iOP5hPtKHNlbJWup_r3g/
 
